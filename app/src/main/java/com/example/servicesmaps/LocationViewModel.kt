afea1b8a12a12ad221e5bdcb440214f943a8e48b
Faizan Ahmed <5ahmef94@solent.ac.uk>
2023-10-09 12:40:55 +0100

    Added stuff to the ViewModel in order for the application to run properly.

--- app/src/main/java/com/example/servicesmaps/LocationViewModel.kt
+++ app/src/main/java/com/example/servicesmaps/LocationViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.servicesmaps
 
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 // You'll find these imports useful for the network communication and JSON parsing
@@ -9,5 +10,10 @@ data class LatLon(var lat: Double=0.0, var lon: Double=0.0)
 
 class LocationViewModel : ViewModel(){
     // Create a latLon property (of type LatLon) and corresponding LiveData, as last week
-
+    var latlon = LatLon(0.0,0.0)
+        set(newLocation) {
+            field = newLocation
+            liveLatLon.value = newLocation
+        }
+    var liveLatLon = MutableLiveData<LatLon>()
 }
